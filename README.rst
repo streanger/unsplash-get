4505eae4ebe06c32693fcf03db95229f34e9089e
streanger <pslisz@o2.pl>
2023-12-07 20:32:48 +0100

    cleanup, docs, image get

--- README.rst
+++ README.rst
@@ -1,16 +1,16 @@
 unsplash_get
-===========
+======================
 Script for scraping unsplash. Only for educational purposes.
 
 Install
-===========
+======================
 
 .. code-block:: python
 
     pip install unsplash_get
 
-Usage from python
-===========
+General usage
+======================
 
 .. code-block:: python
 
@@ -24,3 +24,29 @@ Usage from python
     for key, url in enumerate(urls[:10]):
         file = '{}_{:03}.jpg'.format(word, key)
         save_img(url, file)
+
+Load image to variable
+======================
+
+.. code-block:: python
+
+    import io
+    import numpy as np
+    from PIL import Image
+    from unsplash_get import search, load_img
+
+    def load_img_to_pil(data):
+        img = Image.open(io.BytesIO(data))
+        return img
+
+    def load_image_to_numpy(data):
+        img = np.array(Image.open(io.BytesIO(data)))
+        return img
+
+    # get list of urls
+    word = 'orange'
+    urls = search(word)
+
+    # read image
+    img = load_img(urls[0])
+    print(img.shape)
--- unsplash_get/unsplash_get.py
+++ unsplash_get/unsplash_get.py
@@ -1,23 +1,30 @@
-'''script for scraping unsplash; only for educational purposes'''
-import sys
+"""script for scraping unsplash; only for educational purposes"""
 import os
-import requests
+import sys
 import lxml
+import requests
 from bs4 import BeautifulSoup as bs
 
 
-def save_img(url, path):
-    '''save image from specified url, to specified local path'''
+def save_img(url: str, path: str) -> bool:
+    """save image from specified url, to specified local path"""
     response = requests.get(url)
     if response.status_code != 200:
         return False
     with open(path, 'wb') as f:
         f.write(response.content)
     return True
-    
-    
-def search(phrase):
-    '''get urls of unsplash images for specified phrase'''
+
+
+def get_image(url: str) -> bytes:
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    return response.content
+
+
+def search(phrase: str) -> list:):
+    """get urls of unsplash images for specified phrase"""
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
     search_url = 'https://unsplash.com/s/photos/' + phrase
     res = requests.get(search_url, headers=headers)
@@ -29,24 +36,22 @@ def search(phrase):
     post_script = '/download?force=true'
     urls = [(url + post_script) for url in urls]
     return urls
-    
-    
+
+
 def example():
-    '''example of use'''
+    """example of use"""
     current_path = os.path.realpath(os.path.dirname(sys.argv[0]))
     os.chdir(current_path)
-    
     word = 'cherry'
     urls = search(word)
     print('word: {}, urls number: {}'.format(word, len(urls)))
-    
+
     for key, url in enumerate(urls[:3]):
         out = '{}_{:03}.jpg'.format(word, key)
         print('{:03}.{} -> {}'.format(key, url, out))
         save_img(url, out)
-        
     return None
-    
-    
+
+
 if __name__ == "__main__":
     example()
